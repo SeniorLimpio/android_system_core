2fe78c2b688f00ff05f6a88af6fd6497d8a2dfa9
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2014-05-02 16:28:48 +0100

    sdcard: Link to cutils and libc as DSOs
    There's no reason to link this statically, especially since we're
    building a shared binary anyway. Reduce binary size from 108k to 18k
    
    Change-Id: I0622f039080fa088348570048314121ea4e14587
    
    Conflicts:
    	sdcard/Android.mk

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -6,6 +6,6 @@ LOCAL_SRC_FILES:= sdcard.c
 LOCAL_MODULE:= sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter
 
+LOCAL_STATIC_LIBRARIES := libsdcard
 LOCAL_SHARED_LIBRARIES := libc libcutils
-
 include $(BUILD_EXECUTABLE)
