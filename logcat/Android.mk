3c1b751d32c602083f70687086809aeb384cc9cc
broodplank <guichardflorent@gmail.com>
2014-06-01 04:23:20 +0200

    Provide logdog for cat haters
    Make additional logcat symlinks:
    - logdog
    - loldog
    
    Also provide adb shortcuts:
    - adb logdog
    - adb loldog
    - adb longdog
    
    Change-Id: I99635da7615fe183bf4eca380945cb8a7cee3cfa
    
    Conflicts:
    	logcat/Android.mk

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -10,3 +10,15 @@ LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_MODULE:= logcat
 
 include $(BUILD_EXECUTABLE)
+
+SYMLINKS := $(TARGET_OUT)/bin/lolcat
+SYMLINKS += $(TARGET_OUT)/bin/logdog
+SYMLINKS += $(TARGET_OUT)/bin/loldog
+$(SYMLINKS): LOGCAT_BINARY := $(LOCAL_MODULE)
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(LOGCAT_BINARY)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf $(LOGCAT_BINARY) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
